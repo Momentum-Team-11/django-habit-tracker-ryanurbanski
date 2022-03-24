ac34b948c3b377c6a89e6004d78d8dd23a95cf7f
Ryan Urbanski <36831427+ryanurbanski@users.noreply.github.com>
2022-03-23 21:42:28 -0400

    Delete Habit directory

--- Habit/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User, Habit, Record
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Habit)
-admin.site.register(Record)
